b47cd132bc1725ad6b7f78aa822fe0dad0101b84
dclark <dclark@student.42.fr>
2020-11-29 21:08:51 +0100

    memccpy resolv ?

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 02:15:16 by dclark            #+#    #+#             */
-/*   Updated: 2020/11/17 02:15:18 by dclark           ###   ########.fr       */
+/*   Updated: 2020/11/29 21:08:30 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	unsigned char *d;
 	unsigned char *s;
 
-	d = dest;
+	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	while (0 < n--)
 	{
